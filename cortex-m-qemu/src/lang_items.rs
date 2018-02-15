4c0e41f460529b3dbb2f4e1a873fee4c61b9a492
Paolo Teti <paolo.teti@gmail.com>
2018-02-15 18:36:44 +0100

    Fix start signature adding Termination
    This is enough to avoid this ICE:
    
    error: internal compiler error: librustc_metadata/cstore_impl.rs:131: get_optimized_mir: missing
    MIR forDefId(3/0:10 ~ utest_cortex_m_qemu[f0f1]::lang_items[0]::start[0])

--- cortex-m-qemu/src/lang_items.rs
+++ cortex-m-qemu/src/lang_items.rs
@@ -1,14 +1,26 @@
 use core::intrinsics;
 
 #[lang = "start"]
-extern "C" fn start(main: fn(),
-                    _argc: isize,
-                    _argv: *const *const u8)
-                    -> isize {
+extern "C" fn start<T>(main: fn() -> T, _argc: isize, _argv: *const *const u8) -> isize
+where
+    T: Termination,
+{
     main();
 
     0
 }
+
+#[lang = "termination"]
+pub trait Termination {
+    fn report(self) -> i32;
+}
+
+impl Termination for () {
+    fn report(self) -> i32 {
+        0
+    }
+}
+
 
 #[lang = "panic_fmt"]
 unsafe extern "C" fn panic_fmt(args: ::core::fmt::Arguments,
