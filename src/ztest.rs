d42b877d0c46a135de1a7e2cf4c4dc47afbd7045
Malachi Austin <malachiaustin02@gmail.com>
2020-02-23 20:21:16 -0500

    Added test_runner

--- src/ztest.rs
+++ src/ztest.rs
@@ -1,4 +1,12 @@
 #![no_std]
+#![cfg_attr(feature = "const_fn", feature(const_fn))]
+#![cfg_attr(feature = "const_fn", feature(const_in_array_repeat_expressions))]
+#![cfg_attr(feature = "inline_asm", feature(asm))]
+#![cfg_attr(feature = "abi_x86_interrupt", feature(abi_x86_interrupt))]
+#![cfg_attr(feature = "deny-warnings", deny(warnings))]
+#![cfg_attr(feature = "deny-warnings", deny(missing_docs))]
+#![cfg_attr(not(feature = "deny-warnings"), warn(missing_docs))]
+#![deny(missing_debug_implementations)]
 
 #[doc(hidden)]
 
@@ -7,74 +15,74 @@
 pub static mut __TEST_PANICKED: bool = false;
 
 pub fn test_main_static(tests: &[TestDescAndFn]) {
-    #[allow(improper_ctypes)]
-    extern "Rust" {
-        fn __test_before_run(name: &str);
-        fn __test_failed(name: &str);
-        fn __test_ignored(name: &str);
-        fn __test_start(ntests: usize);
-        fn __test_success(name: &str);
-        fn __test_summary(passed: usize, failed: usize, ignored: usize);
-    }
+  #[allow(improper_ctypes)]
+  extern "Rust" {
+    fn __test_before_run(name: &str);
+    fn __test_failed(name: &str);
+    fn __test_ignored(name: &str);
+    fn __test_start(ntests: usize);
+    fn __test_success(name: &str);
+    fn __test_summary(passed: usize, failed: usize, ignored: usize);
+  }
 
-    unsafe {
-        __test_start(tests.len());
+  unsafe {
+    __test_start(tests.len());
 
-        let mut failed = 0;
-        let mut ignored = 0;
-        let mut passed = 0;
-        for test in tests {
-            if test.desc.ignore {
-                ignored += 1;
-                __test_ignored(test.desc.name.0);
-            } else {
-                __test_before_run(test.desc.name.0);
+    let mut failed = 0;
+    let mut ignored = 0;
+    let mut passed = 0;
+    for test in tests {
+      if test.desc.ignore {
+        ignored += 1;
+        __test_ignored(test.desc.name.0);
+      } else {
+        __test_before_run(test.desc.name.0);
 
-                __TEST_PANICKED = false;
+        __TEST_PANICKED = false;
 
-                test.testfn.0();
+        test.testfn.0();
 
-                if __TEST_PANICKED ==
-                   (test.desc.should_panic == ShouldPanic::Yes) {
-                    passed += 1;
-                    __test_success(test.desc.name.0);
-                } else {
-                    failed += 1;
-                    __test_failed(test.desc.name.0);
-                }
-            }
-
+        if __TEST_PANICKED ==
+          (test.desc.should_panic == ShouldPanic::Yes) {
+          passed += 1;
+          __test_success(test.desc.name.0);
+        } else {
+          failed += 1;
+          __test_failed(test.desc.name.0);
         }
+      }
 
-        __test_summary(passed, failed, ignored);
     }
+
+    __test_summary(passed, failed, ignored);
+  }
 }
 
 pub trait Termination {
-    fn report(self) -> i32;
+  fn report(self) -> i32;
 }
 
 impl Termination for () {
-    fn report(self) -> i32 {
-        0
-    }
+  fn report(self) -> i32 {
+    0
+  }
 }
 
 pub fn assert_test_result<T: Termination>(result: T) {
-    assert_eq!(result.report(), 0);
+  assert_eq!(result.report(), 0);
 }
 
 // required for compatibility with the `rustc --test` interface
 pub struct TestDescAndFn {
-    pub desc: TestDesc,
-    pub testfn: StaticTestFn,
+  pub desc: TestDesc,
+  pub testfn: StaticTestFn,
 }
 
 pub struct TestDesc {
-    pub allow_fail: bool,
-    pub ignore: bool,
-    pub name: StaticTestName,
-    pub should_panic: ShouldPanic,
+  pub allow_fail: bool,
+  pub ignore: bool,
+  pub name: StaticTestName,
+  pub should_panic: ShouldPanic,
 }
 
 pub struct StaticTestName(pub &'static str);
@@ -82,6 +90,14 @@ pub struct StaticTestFn(pub fn());
 
 #[derive(PartialEq)]
 pub enum ShouldPanic {
-    No,
-    Yes,
+  No,
+  Yes,
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+  println!("Running {} tests", tests.len());
+  for test in tests {
+    test();
+  }
 }
